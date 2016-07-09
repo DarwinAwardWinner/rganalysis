8abcb25ef8a50998f35275cf9cd50f478cfc115a
Ryan C. Thompson <rct@thompsonclan.org>
2016-07-09 15:51:13 -0700

    Finish packaging
    Now rganalysis can be installed using pip.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name='rganalysis',
+    version='3.0',
+    description='A script to add ReplayGain tags to your music library',
+    url='https://github.com/DarwinAwardWinner/rganalysis',
+    author='Ryan C. Thompson',
+    author_email='rct+rganalysis@thompsonclan.org',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='audio replaygain',
+    packages=find_packages(exclude=['scripts']),
+    install_requires=[
+        'audiotools',
+        'mutagen',
+        'plac',
+    ],
+    scripts=['scripts/rganalysis',],
+)
